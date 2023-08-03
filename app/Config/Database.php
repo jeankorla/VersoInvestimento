c7b1311c134ae55045a6b3542d4b8b5abbd36af8
Fragoso <felipefragoso@grupogelog.com.br>
2023-08-02 22:52:07 -0300

    Configurações Servidor

--- app/Config/Database.php
+++ app/Config/Database.php
@@ -27,9 +27,9 @@ class Database extends Config
     public array $default = [
         'DSN'      => '',
         'hostname' => '50.116.86.134',
-        'username' => 'investpl_jean',
-        'password' => 'C0mpany!SC10',
-        'database' => 'investpl',
+        'username' => 'investpl_x',
+        'password' => 'Jean1509!',
+        'database' => 'investpl_viver_renda',
         'DBDriver' => 'MySQLi',
         'DBPrefix' => '',
         'pConnect' => false,
