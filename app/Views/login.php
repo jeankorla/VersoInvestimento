f1e8e3e908632db639ca0597efb71b746c44487a
JeanKorla <jeansilvakorla@hotmail.com>
2023-10-24 11:05:31 -0300

    caminho login corrigido

--- app/Views/login.php
+++ app/Views/login.php
@@ -47,7 +47,7 @@
             <div class="card-body py-5 px-md-5">
               <h1 class="text-center text-white">Login</h1>
 
-              <form action="<?php echo base_url('autenticacao/login') ?>" method="post">
+              <form action="<?php echo base_url('/login') ?>" method="post">
                 <!-- Email input -->
                 <div class="form-outline mb-4">
                   <input type="text" id="NAME" name="NAME" class="form-control" />
