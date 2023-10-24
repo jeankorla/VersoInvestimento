9a21c35a04493b1649aeca247a9190715ba427c7
JeanKorla <jeansilvakorla@hotmail.com>
2023-10-24 11:40:36 -0300

    resposnividade

--- app/Views/administrate.php
+++ app/Views/administrate.php
@@ -1,9 +1,14 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
   <meta charset="UTF-8">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <title>Admin</title>
+   <!-- css -->
+  <link rel="stylesheet" href="style/style.css">
+  <link rel="stylesheet" href="style/responsividade.css">
+  
   <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
   <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/css/all.min.css" />
   <link rel="preconnect" href="https://fonts.googleapis.com">
@@ -13,9 +18,7 @@
   
   <script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
 
-  <!-- css -->
-  <link rel="stylesheet" href="style/style.css">
-  <link rel="stylesheet" href="style/responsividade.css">
+ 
 
   
   
