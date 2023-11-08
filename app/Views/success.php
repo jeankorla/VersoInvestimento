14ee7c5266281ee50291e76df0dc3f08762bb335
JeanKorla <jeansilvakorla@hotmail.com>
2023-11-08 10:13:20 -0300

    css success

--- app/Views/success.php
+++ app/Views/success.php
@@ -8,8 +8,9 @@
   <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
   <link href="https://fonts.googleapis.com/css2?family=Montserrat:wght@500;600;700;900&display=swap" rel="stylesheet">
 
-  <!---- ---->
-  <link rel="stylesheet" href="style/responsividade.css">
+   <!-- CSS -->
+    <link rel="stylesheet" href="style/style.css">
+    <link rel="stylesheet" href="style/responsividade.css">
   
 
   <title>Navbar</title>
